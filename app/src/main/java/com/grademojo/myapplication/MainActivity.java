622ae0c9555f0aa56b1f46fe8e783197bd9e4630
manit <ermanit007@gmail.com>
2017-07-10 10:53:48 +0530

    add button

--- app/src/main/java/com/grademojo/myapplication/MainActivity.java
+++ app/src/main/java/com/grademojo/myapplication/MainActivity.java
@@ -2,8 +2,10 @@ package com.grademojo.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button cq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
